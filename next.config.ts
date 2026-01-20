d7b93091729c39c492c012364f0b50a6bb8d8e54
songxiaoxu <18382210583@163.com>
2026-01-20 17:51:54 +0800

    fix: 解决项目无法热更新

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    // config.watchOptions = {
-    //   poll: 1000,
-    //   aggregateTimeout: 300,
-    // };
-    return config;
-  },
 };
 
 export default nextConfig;
